65bf2a66c204de53f3633dc9d775bfc8d4a51bed
okezino <okezi002@gmail.com>
2021-08-30 21:43:00 +0100

    chat

--- app/src/main/java/com/plcoding/streamchatapp/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/plcoding/streamchatapp/ui/channel/ChannelFragment.kt
@@ -7,11 +7,13 @@ import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.plcoding.streamchatapp.R
 import com.plcoding.streamchatapp.databinding.FragmentChannelBinding
 import com.plcoding.streamchatapp.ui.BindingFragment
+import com.plcoding.streamchatapp.util.safeNavigation
 import dagger.hilt.android.AndroidEntryPoint
 import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.ui.channel.list.header.viewmodel.ChannelListHeaderViewModel
@@ -19,6 +21,7 @@ import io.getstream.chat.android.ui.channel.list.header.viewmodel.bindView
 import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
 import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
 import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class ChannelFragment : BindingFragment<FragmentChannelBinding>() {
@@ -45,10 +48,37 @@ class ChannelFragment : BindingFragment<FragmentChannelBinding>() {
         val channelListHeaderViewModel : ChannelListHeaderViewModel by viewModels()
         channelListViewModel.bindView(binding.channelListView, viewLifecycleOwner)
         channelListHeaderViewModel.bindView(binding.channelListHeaderView, viewLifecycleOwner)
+
         binding.channelListHeaderView.setOnUserAvatarClickListener{
             Toast.makeText(requireActivity(), "Good", Toast.LENGTH_SHORT).show()
             viewModel.logout()
             findNavController().popBackStack()
+        }
+        binding.channelListHeaderView.setOnActionButtonClickListener{
+            findNavController().safeNavigation(R.id.action_channelFragment_to_createChannelDialog)
+        }
+
+        binding.channelListView.setChannelItemClickListener{
+            findNavController().safeNavigation(R.id.action_channelFragment_to_chatFragment,
+                Bundle().apply {
+                    putString("channelId", it.cid)
+                })
+
+        }
+
+        lifecycleScope.launchWhenStarted {
+          viewModel.createChannelEvent.collect { event ->
+              when(event){
+                  is ChannelViewModel.CreateChannel.Error -> {
+                      Toast.makeText(requireContext(), event.message, Toast.LENGTH_SHORT).show()
+                  }
+                  is ChannelViewModel.CreateChannel.Success ->{
+                      Toast.makeText(requireContext(), R.string.channel_created, Toast.LENGTH_SHORT).show()
+                  }
+
+              }
+          }
+
         }
 
 
--- app/src/main/java/com/plcoding/streamchatapp/ui/channel/ChannelViewModel.kt
+++ app/src/main/java/com/plcoding/streamchatapp/ui/channel/ChannelViewModel.kt
@@ -1,21 +1,59 @@
 package com.plcoding.streamchatapp.ui.channel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.call.await
 import io.getstream.chat.android.client.models.User
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class ChannelViewModel @Inject constructor(
-    private val client : ChatClient
-): ViewModel() {
+    private val client: ChatClient
+) : ViewModel() {
 
-    fun logout(){
+    private val _createChannelEvent = MutableSharedFlow<CreateChannel>()
+    val createChannelEvent : SharedFlow<CreateChannel>
+        get() = _createChannelEvent
+
+
+
+    fun createChannel(name: String) {
+
+        viewModelScope.launch {
+            if (name.trim().isEmpty()) {
+                _createChannelEvent.emit(CreateChannel.Error("Cant create and Empty Channel"))
+                return@launch
+            }
+
+
+            var result = client.channel("messaging", UUID.randomUUID().toString())
+                .create(mapOf("name" to name.trim())).await()
+
+            if(result.isError){
+                _createChannelEvent.emit(CreateChannel.Error(result.error().message ?: "Something when Wrong"))
+                return@launch
+            }
+
+            _createChannelEvent.emit(CreateChannel.Success)
+
+        }
+    }
+
+    fun logout() {
         client.disconnect()
     }
 
     fun getCurrentUser(): User? {
         return client.getCurrentUser()
+    }
+    sealed class CreateChannel{
+        data class Error(val message : String) : CreateChannel()
+        object Success : CreateChannel()
     }
 }
